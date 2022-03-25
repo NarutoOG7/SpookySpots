548b75b2f883271bb4acb1e2e8bf07acf398fa58
Spencer Belton <sbelton@Spencers-MacBook-Air.local>
2022-03-24 20:39:14 -0600

    Creating the project

---
+++ SpookySpots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpookySpots
+//
+//  Created by Spencer Belton on 3/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpookySpots/SpookySpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpookySpotsApp.swift
+//  SpookySpots
+//
+//  Created by Spencer Belton on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SpookySpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
